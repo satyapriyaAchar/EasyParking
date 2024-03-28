9b9c7be263979f5ee54a8ceff219d29ec3d382fb
Satyapriya <151806611+satyapriyaAchar@users.noreply.github.com>
2024-03-28 22:16:27 +0530

    contact details pop up make dynamic

--- admin/settings.php
+++ admin/settings.php
@@ -37,7 +37,7 @@
               <p class="card-text" id="site_about"></p>
             </div>
           </div>
-          <!------- popup for edit dynamically ------->
+          <!------- popup for edit dynamically(modal)for title and about ------->
           <div class="modal fade" id="general-s" data-bs-backdrop="static" data-bs-keyboard="true" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
             <div class="modal-dialog">
               <form id="general_s_form">
@@ -96,23 +96,28 @@
                 <div class="col-lg-6">
                   <div class="mb-4">
                     <h6 class="card-subtitle mb-1 fw-bold">Address</h6>
-                    <p class="card-text" id="address"></p>
+                    <p class="card-text"></p>
+                    <i class="fa-solid fa-address-card" style="color: green;"></i>
+                    <span id="address"></span>
                   </div>
                   <div class="mb-4">
                     <h6 class="card-subtitle mb-1 fw-bold" >Google Map</h6>
-                    <p class="card-text" id="gmap"></p>
+                    <p class="card-text"></p>
+                    <i class="fa-solid fa-map-location-dot" style="color: blue"></i>
+                    <span id="gmap"></span>
                   </div>
                   <div class="mb-4">
                     <h6 class="card-subtitle mb-1 fw-bold" >Phone Number</h6>
-                    <p class="card-text" id="pn1">
+                    <p class="card-text">
                     <i class="fa-solid fa-phone" style="color: green;" aria-hidden="true"></i>
-                    <!-- <span id="pn1"></span> -->
+                    <span id="pn1"></span>
                     </p>
                   </div>
                   <div class="mb-4">
                     <h6 class="card-subtitle mb-1 fw-bold" >Email</h6>
-                    <p class="card-text" id="email">
+                    <p class="card-text">
                     <i class="fa-solid fa-envelope" style="color: orange;" aria-hidden="true"></i>
+                    <span id="email"></span>
                     </p>
                   </div>
                 </div>
@@ -121,17 +126,21 @@
                 <div class="col-lg-6">
                   <div class="mb-4">
                     <h6 class="card-subtitle mb-1 fw-bold" >Social Links</h6>
-                    <p class="card-text mb-1" id="linkd">
+                    <p class="card-text mb-1" >
                       <i class="fa-brands fa-linkedin" style="color: blue;" aria-hidden="true"></i>
+                      <span id="linkd"></span>
                     </p>
-                    <p class="card-text mb-1" id="tw">
+                    <p class="card-text mb-1">
                       <i class="fa-brands fa-twitter" aria-hidden="true"></i>
+                      <span id="tw"></span>
                     </p>
-                    <p class="card-text mb-1" id="yt">
+                    <p class="card-text mb-1">
                       <i class="fa-brands fa-youtube" style="color: red;" aria-hidden="true"></i>
+                      <span id="yt"></span>
                     </p>
-                    <p class="card-text mb-1" id="wp">
+                    <p class="card-text mb-1">
                       <i class="fa-brands fa-whatsapp" style="color: green;" aria-hidden="true"></i> 
+                      <span id="wp"></span>
                     </p>
                   </div>
                   <div class="mb-4">
@@ -143,6 +152,108 @@
             </div>
           </div>
 
+          <!------- popup for edit dynamically(modal) for contact ------->
+          <div class="modal fade" id="contacts-s" data-bs-backdrop="static" data-bs-keyboard="true" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+            <div class="modal-dialog modal-lg">
+              <form id="general_s_form">
+                <div class="modal-content">
+                  <div class="modal-header">
+                    <h5 class="modal-title">Contacts Settings</h5>
+                  </div>
+                  <div class="modal-body">
+                    <div class="container-fluid p-0">
+                      <div class="row">
+                        <div class="col-md-6">
+                            <div class="mb-3">
+                              <label class="form-label fw-bold">Address</label>
+                              <div class="input-group mb-3">
+                                <span class="input-group-text">
+                                  <i class="fa-solid fa-address-card" style="color: green;"></i>
+                                </span>
+                                <input type="text" name="address" id="address_inp" class="form-control shadow-none" required>
+                              </div>
+                            </div>
+                            <div class="mb-3">
+                              <label class="form-label fw-bold">Google Map Link</label>
+                              <div class="input-group mb-3">
+                                <span class="input-group-text">
+                                  <i class="fa-solid fa-map-location-dot" style="color: blue"></i>
+                                </span>
+                                <input type="text" name="gmap" id="gmap_inp" class="form-control shadow-none" required>
+                              </div>
+                            </div>
+                            <div class="mb-3">
+                              <label class="form-label fw-bold">Phone Number</label>
+                              <div class="input-group mb-3">
+                                <span class="input-group-text">
+                                  <i class="fa-solid fa-phone" style="color: green;" aria-hidden="true"></i>
+                                </span>
+                                <input type="text" name="pn1" id="pn1_inp" class="form-control shadow-none" required>
+                              </div>
+                            </div>
+                            <div class="mb-3">
+                              <label class="form-label fw-bold">Email</label>
+                              <div class="input-group mb-3">
+                                <span class="input-group-text">
+                                  <i class="fa-solid fa-envelope" style="color: orange;" aria-hidden="true"></i>
+                                </span>
+                                <input type="email" name="email" id="email_inp" class="form-control shadow-none" required>
+                              </div>
+                            </div>
+
+                        </div>
+                        <div class="col-md-6">
+                          <div class="mb-3">
+                            <label class="form-label fw-bold">Social Links</label>
+                            <div class="input-group mb-3">
+                              <span class="input-group-text">
+                                <i class="fa-brands fa-linkedin" style="color: blue;" aria-hidden="true"></i>
+                              </span>
+                              <input type="text" name="linkd" id="linkd_inp" class="form-control shadow-none" required>
+                            </div>
+                            <div class="input-group mb-3">
+                              <span class="input-group-text">
+                                <i class="fa-brands fa-twitter" aria-hidden="true"></i>
+                              </span>
+                              <input type="text" name="tw" id="tw_inp" class="form-control shadow-none" required>
+                            </div>
+                            <div class="input-group mb-3">
+                              <span class="input-group-text">
+                                <i class="fa-brands fa-youtube" style="color: red;" aria-hidden="true"></i>
+                              </span>
+                              <input type="text" name="yt" id="yt_inp" class="form-control shadow-none" required>
+                            </div>
+                            <div class="input-group mb-3">
+                              <span class="input-group-text">
+                                <i class="fa-brands fa-whatsapp" style="color: green;" aria-hidden="true"></i> 
+                              </span>
+                              <input type="text" name="wp" id="wp_inp" class="form-control shadow-none" required>
+                            </div>
+                          </div>
+                          <div class="mb-3">
+                            <label class="form-label fw-bold">iFrame src</label>
+                            <input type="text" name="iframe" id="iframe_inp" class="form-control shadow-none" required>
+                          </div>
+                        </div>
+                      </div>
+                    </div>
+                    <!-- <div class="mb-3">
+                      <label class="form-label fw-bold">Address</label>
+                      <input type="text" name="site_title" id="address_inp" class="form-control shadow-none" required>
+                    </div>
+                    <div class="mb-3">
+                      <label class="form-label fw-bold">Address</label>
+                      <textarea name="site_about" id="address_inp" class="form-control shadow-none" rows="6" required></textarea>
+                    </div> -->
+                  </div>
+                  <div class="modal-footer">
+                    <button type="button" onclick="site_title.value = general_data.site_title, site_about.value =general_data.site_about" class="btn text-secondary shadow-none" data-bs-dismiss="modal">Cancel</button>
+                    <button type="submit" class="btn btn-primary shadow-none">Save</button>
+                  </div>
+                </div>
+              </form>
+            </div>
+          </div>
         </div>
       </div>
     </div>
@@ -264,7 +375,14 @@
         
         xhr.onload = function(){
           contacts_data = JSON.parse(this.responseText);
-          console.log(contacts_data);
+          contacts_data = Object.values(contacts_data);
+          // console.log(contacts_data);
+
+          for(i=0 ; i<contacts_p_id.length ; i++)
+          {
+            document.getElementById(contacts_p_id[i]).innerText = contacts_data[i+1];
+          }
+          iframe.src = contacts_data[(contacts_p_id.length)+1];
           
 
         }
