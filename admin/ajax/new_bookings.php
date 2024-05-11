11d277c59ac33be54371280cc3cdecce1e2de666
Satyapriya <151806611+satyapriyaAchar@users.noreply.github.com>
2024-05-11 22:43:25 +0530

    new booking

---
+++ admin/ajax/new_bookings.php
@@ -0,0 +1,105 @@
+<?php
+    require('../inc/db_config.php');
+    require('../inc/essential.php');
+    adminLogin();
+
+  
+    if(isset($_POST['get_bookings']))
+    {
+        $frm_data = filteration($_POST);
+
+        $query = "SELECT bo.*,bd.* FROM `booking_order` bo
+            INNER JOIN `booking_details` bd ON bo.booking_id = bd.booking_id
+            WHERE (bo.order_id LIKE ? OR bd.phonenum LIKE ? OR bd.user_name LIKE ?)
+            AND (bo.booking_status = ? AND bo.arrival = 0) ORDER BY bo.booking_id ASC";
+
+        $res = select($query,
+            ["%$frm_data[search]%","%$frm_data[search]%","%$frm_data[search]%","booked",0],'sssss');
+        $i = 1;
+        $table_data = "";
+
+        if(mysqli_num_rows($res)==0){
+            echo"<b>No Data Found!</b>";
+            exit;
+        }
+
+        while($data = mysqli_fetch_assoc($res))
+        {
+            $data = date("d-m-Y",strtotime($data['datentime']));
+            $checkin = date("d-m-Y",strtotime($data['check_in']));
+            $checkout = date("d-m-Y",strtotime($data['check_out']));
+
+
+            $table_data .="
+                <tr>
+                    <td>
+                        <span class='badge bg-primary'>
+                            Order Id: $data[order_id]
+                        </span>
+                        <br>
+                        <b>Name :</b> $data[user_name]
+                        <br>
+                        <b>Phone :</b> $data[phonenum]
+                        <br>
+                        <b>Name :</b> $data[user_name]
+                    </td>
+                    <td>
+                        <b>Parking :</b> $data[parking_name]
+                        <br>
+                        <b>Price :</b> ₹$data[price]
+                    </td>
+                    <td>
+                        <b>Check-in:</b> $checkin
+                        <br>
+                        <b>Check-out:</b> $checkout
+                        <br>
+                        <b>Paid :</b> ₹$data[trans_amt]
+                        <br>
+                        <b>Date:</b> $date
+                    </td>
+                    <td>
+                        <button type='button' onclick='assign_parking($data[booking_id])' class='btn text-white btn-sm fw-bold custom-bg shadow-none' data-bs-toggle='modal' data-bs-target='#assign-parking'>
+                            <i class='bi bi-check2-square'></i> Assign parking
+                        </button>
+                        <br>
+                        <button type='button' onclick='cancel_booking($data[booking_id])' class='mt-2 btn btn-outline-danger btn-sm fw-bold shadow-none'>
+                            <i class='bi bi-trash'></i> Cancel Booking
+                        </button>
+                    </td>
+                </tr>
+            ";
+            $i++;
+        }
+        echo $table_data;
+    }
+
+    if(isset($_POST['assign_parking']))
+    {
+        $frm_data = filteration($_POST);
+
+        $query = "UPDATE `booking_order` bo INNER JOIN `booking_details` bd
+            ON bo.booking_id = bd.booking_id
+            SET bo.arrival = ?, bd.parking_no= ?
+            WHERE bo.booking_id = ?";
+    
+        $value = [1,$frm_data['parking_no'],$frm_data['booking_id']];
+        $res = update($query,$value,'isi'); // it will update 2 rows so it will return 2
+
+        echo ($res==2) ? 1 : 0 ;
+
+    }
+
+    if(isset($_POST['cancel_booking']))
+    {
+        $frm_data = filteration($_POST);
+
+        $query = "UPDATE `booking_order` SET `booking_status`=?, `refund`=? WHERE bo.booking_id = ?";
+        $value = ['cancelled',0,$frm_data['booking_id']];
+        $res = update($query,$value,'ssi');
+
+        echo $res;
+       
+    }
+
+
+?>
---
+++ admin/scripts/new_bookings.js
@@ -0,0 +1,83 @@
+
+    function get_bookings(search='')
+    {
+        let xhr = new XMLHttpRequest();
+            xhr.open("POST","ajax/new_bookings.php",true);
+            xhr.setRequestHeader('Content-Type','application/x-www-form-urlencoded') ;
+
+            xhr.onload = function(){
+            document.getElementById('table-data').innerHTML = this.responseText;
+            }
+            xhr.send('get_bookings&search='+search);
+    }
+
+    let assign_parking_form = document.getElementById('assign_parking_form');
+
+    function assign_parking(id)
+    {
+        assign_parking_form.elements['booking_id'].value=id;    
+    }
+    assign_parking_form.addEventListener('submit',function(e){
+        e.preventDefault();
+
+        let data = new FormData();
+        // console.log(services_s_form.elements['services_name'].value);
+        data.append('parking_no',assign_parking_form.elements['parking_no'].value);
+        data.append('booking_id',assign_parking_form.elements['booking_id'].value);    
+        data.append('assign_parking');
+    
+        let xhr = new XMLHttpRequest();
+        xhr.open("POST","ajax/new_bookings.php",true);
+    
+        xhr.onload = function(){
+        // console.log(this.responseText);
+    
+            var myModal = document.getElementById('assign-parking');
+            var modal = bootstrap.Modal.getInstance(myModal);
+            modal.hide();
+
+            if(this.responseText == 1)
+            {
+                alert('success','Parking number alloted! Booking finalized');
+                assign_parking_form.reset();
+                get_bookings();
+            }
+            else{
+                alert('error','server down')
+            }
+        }
+        xhr.send(data);
+    });
+
+
+    function cancel_booking(id)
+    {
+        if(confirm("Are you sure, you want to cancel this booking?"))
+        {
+            let data = new FormData();
+            data.append('booking_id',id);
+            data.append('cancel_booking','');
+
+            let xhr = new XMLHttpRequest();
+            xhr.open("POST","ajax/new_bookings.php",true);
+
+            xhr.onload = function()
+            {
+                if(this.responseText == 1)
+                {
+                    alert('success','Booking Cancelled!');
+                    get_bookings();           
+                }
+                else
+                {
+                    alert('error','Sever Down!');
+                }
+            }
+            xhr.send(data);
+        }
+    }
+
+
+    window.onload = function(){
+        get_bookings();
+    }
